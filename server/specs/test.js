5fc88d61276e7eccecfa70bf78760f84918749e0
Osuh Emmanuel <mrmusonant@gmail.com>
2018-10-19 20:44:27 +0100

    Add more tests for Product create requests

--- server/specs/test.js
+++ server/specs/test.js
@@ -6,7 +6,7 @@ import modelTests from './models/test';
 // Run Tests for the Model Class and Data Model Classes
 modelTests();
 
-// const wrongToken = 'OPsdecedsaec00e2349__dL';
+const wrongToken = 'OPsdecedsaec00e2349__dL';
 const attendantToken = 'OPsdecedsaec00e23490LdL';
 // const wrongAttendantToken = 'KLcxdwedsaecd3e23490LdL';
 // const attendantSaleId = 1;
@@ -69,9 +69,30 @@ describe('All test cases for Store Manager API', () => {
           done();
         });
     });
-    it('Should create a products', (done) => {
+    it('Should not create product with wrong token sent', (done) => {
       request.post('/api/v1/products')
-        .set('x-access-token', 'xcmr2ewesaec00e23490LdL')
+        .set('x-access-token', wrongToken)
+        .send(productData)
+        .expect(401)
+        .end((err, res) => {
+          expect(res.body.message).to.equal('wrong token');
+          if (err) done(err);
+          done();
+        });
+    });
+    it('Should not create product with token missing from request', (done) => {
+      request.post('/api/v1/products')
+        .send(productData)
+        .expect(401)
+        .end((err, res) => {
+          expect(res.body.message).to.equal('missing token');
+          if (err) done(err);
+          done();
+        });
+    });
+    it('Should create a products with satisfactory requirements', (done) => {
+      request.post('/api/v1/products')
+        .set('x-access-token', ownerToken)
         .send(productData)
         .expect(200)
         .end((err, res) => {
